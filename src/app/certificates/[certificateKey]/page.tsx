5dec8fde00e9c821df167afd94fd41df32f859f9
sagnikghosh1999 <saggywebd@gmail.com>
2024-12-25 19:50:18 +0530

    fixing bloated SEO content

--- src/app/certificates/[certificateKey]/page.tsx
+++ src/app/certificates/[certificateKey]/page.tsx
@@ -49,10 +49,6 @@ export async function generateMetadata(
     description: certificate?.description,
     category: `${CERTIFICATES_PAGE?.label}`,
     creator: developerName,
-    keywords: [
-      certificate?.name,
-      ...certificate?.skills.map((skill) => skillDatabaseMap[skill]?.name),
-    ],
   };
 }
 
